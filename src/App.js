a81b09579e859f85f5794b2f1d01fceaabc8c801
Antonio David Viniski <davidviniski@gmail.com>
2023-09-03 08:45:01 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [listItems, setCurrentTime] = useState(0);
+  
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.map((row) => <p>{row.name}</p>));
+    });
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +26,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>The  current time is {listItems}</p>
       </header>
     </div>
   );
